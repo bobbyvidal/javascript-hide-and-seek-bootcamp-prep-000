6d2a15de61826c630115458621f061d093629cd4
Robert Vidal <bobbyvidal@gmail.com>
2018-10-19 23:41:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+httpserver;
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+function increaseRankBy(n) {
+  var rankUp = document.getElementById('app').querySelectorAll('u1.ranked-list');
+  for (let i = 0; i < rankUp.length; i++){
+    rankUp[i].innerHTML = ((parseInt(rankUp[i].innerHTML)) + n)
+    }
+}
